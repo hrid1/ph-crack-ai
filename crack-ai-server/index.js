af9556c3d267305863a151e2e6f4caed9e171371
hrid1 <hridoycse1@gmail.com>
2025-02-01 01:10:45 +0600

    First test-api done

--- crack-ai-server/index.js
+++ crack-ai-server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const port = process.env.PORT || 4000;
 
 const app = express();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // middleware
 app.use(express.json());
+
+// const prompt = "Explain how AI works in short";
+
+app.get("/crack-ai", async (req, res) => {
+  const query = req.query?.prompt;
+  let prompt = query || "Say Greets";
+
+  console.log(prompt);
+
+  const result = await model.generateContent(prompt + "in short");
+  res.send(result.response.text());
+});
 
 app.get("/", (req, res) => {
   res.send("Ai Server is Running!");
